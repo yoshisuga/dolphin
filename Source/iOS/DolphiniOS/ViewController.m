7d1265f8a9fd5fccbca7178b0d028ea7b65c36a4
OatmealDome <theoatmealdome@gmail.com>
2016-01-03 23:08:05 -0500

    iOS: Start emulation when view loads

--- Source/iOS/DolphiniOS/ViewController.m
+++ Source/iOS/DolphiniOS/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Dolphin Emulator Project. All rights reserved.
 //
 
+#import "DolphinWX/MainiOS.h"
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,6 +19,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+	
+	dispatch_queue_t queue = dispatch_queue_create("dolphinQueue", NULL);
+	
+	dispatch_async(queue, ^{
+		[MainiOS StartEmulationWithFile:[[NSBundle mainBundle] pathForResource:@"starfield" ofType:@"dol"] userDirectory:[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]];
+	});
 }
 
 - (void)didReceiveMemoryWarning {
